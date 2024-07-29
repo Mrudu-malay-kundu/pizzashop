eb26f703207f369c70edb9d8ab013f03e0fadf51
Mrudu-malay-kundu <158554220+Mrudu-malay-kundu@users.noreply.github.com>
2024-07-29 21:00:01 +0530

    Add files via upload

---
+++ migrate.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    string name="farm house";
+    char size='m';
+    int personitserve=2;
+    float retailprice=370.8;
+    cout<<"name"<<"\n";
+    cout<<"size"<<"\n";
+    cout<<"personitserve"<<"\n";
+    cout<<"retailprice"<<"\n";
+    cout<<"yay you have order a "<< name <<  " pizza of size " << size <<  " that " "serves "  <<  personitserve <<  " \n pls pay RS. " << retailprice <<  " to delivery person ";
+    return 0;
+
+}
